7d0e653225576b1efa64dbc996cfda556c22b11e
Rahuul Rathod <shivani24121999@gmail.com>
2021-10-01 16:45:35 +0530

    Shivani_UpdateMyBusTickets&MyBusDetails

--- src/app/mybooktickets/mybooktickets.component.ts
+++ src/app/mybooktickets/mybooktickets.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes } from '../_classes/routes';
+import { RoutefetchService } from '../_services/routefetch.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-mybooktickets',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MybookticketsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: RoutefetchService, private router: Router) { }
+
+  RouteObject!: Routes;
+  ReturnRoute!: Routes;
+  RouteId!: number;
+  TicketPrice: number = 0;
+  DepartureDate!: string;
+  tickettype!: string;
 
   ngOnInit(): void {
+    this.RouteObject = new Routes();
+    this.ReturnRoute = new Routes();
+
+  }
+
+  GetMyBuses() {
+    console.log(this.RouteObject);
+    this.service.ServiceMethodGetRouteDetails(this.RouteObject).subscribe(
+      res => {
+        this.RouteId = res.RouteId;
+        this.TicketPrice = res.TicketPrice;
+        if (this.RouteId != null) {
+          console.log("routefetch successful");
+          this.ReturnRoute = res;
+          console.log(this.ReturnRoute);
+          console.log(this.RouteId);
+        }
+        localStorage.setItem('RouteId', JSON.stringify(this.RouteId));
+        localStorage.setItem('TicketPrice', JSON.stringify(this.TicketPrice));
+        localStorage.setItem('DepartureDate', this.DepartureDate);
+        localStorage.setItem('tickettype', this.tickettype);
+
+        if (this.RouteId != 0) {
+          this.router.navigate(['/mybusdetails'])
+        }
+      }
+    );
+
+
   }
 
 }
--- src/app/mybusdetails/mybusdetails.component.ts
+++ src/app/mybusdetails/mybusdetails.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+//Added...
+import { Buses } from '../_classes/buses';
+import { BusesfetchService } from '../_services/busesfetch.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mybusdetails',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MybusdetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private busservice: BusesfetchService) { }
+
+  BusObject!: Buses;
+  RouteId: number = 0;
+  id!: string;
+  DepartureDate!: string;
+  tickettype!: string;
+
+  Buses: any = [];
+  TicketPrice: number = 0;
+
+
 
   ngOnInit(): void {
+    this.BusObject = new Buses();
+    this.id = (localStorage.getItem('RouteId') || '');
+    this.RouteId = parseInt(this.id);
+    this.id = (localStorage.getItem('TicketPrice') || '');
+    this.TicketPrice = parseInt(this.id);
+    this.DepartureDate = (localStorage.getItem('DepartureDate') || '');
+    this.tickettype = (localStorage.getItem('tickettype') || '');
+    this.BusObject.RouteId = this.RouteId;
+    this.BusObject.DepartureDate = this.DepartureDate;
+    this.GetMyBusesDetails();
   }
 
+  GetMyBusesDetails() {
+
+    this.busservice.ServiceMethodGetBusesDetails(this.BusObject).subscribe(data => { this.Buses = data; });
+
+  }
+
+  BookClick() {
+    this.router.navigate(['/seatselection']);
+  }
+
+
 }
+
